631a1df67616d1b406a99022954135fc6860e211
sarim-tanveer-sc <sarim2195@gmail.com>
2026-04-16 20:54:50 +1000

    Update 05_cost_to_revenue.sql

--- sql/kpi/05_cost_to_revenue.sql
+++ sql/kpi/05_cost_to_revenue.sql
@@ -11,23 +11,23 @@
 
 WITH fact_shipment_orderline AS (
     SELECT order_id, order_line_id, 
-			     SUM(shipped_qty) AS shipped_qty_orderline,
-			     SUM(freight_cost) AS freight_cost_orderline
+		   SUM(shipped_qty) AS shipped_qty_orderline,
+		   SUM(freight_cost) AS freight_cost_orderline
     FROM fact_shipment_line
     GROUP BY order_id, order_line_id
 ),
 fact_ship_rev_orderline AS (
     SELECT sh.order_id, sh.order_line_id, sh.shipped_qty_orderline,
-			     sh.freight_cost_orderline, o.dc_id, o.channel, o.unit_price, 
-			     strftime('%W',o.order_created_ts) AS week_num
+		   sh.freight_cost_orderline, o.dc_id, o.channel, o.unit_price, 
+		   strftime('%W',o.order_created_ts) AS week_num
     FROM fact_order_line AS o
     INNER JOIN fact_shipment_orderline AS sh
     ON o.order_id = sh.order_id AND o.order_line_id = sh.order_line_id
 )
 SELECT order_id, dc_id, channel, week_num,
-			 SUM(freight_cost_orderline) AS freight_cost_order,
-			 SUM(unit_price * shipped_qty_orderline) AS revenue_order,
-			 ROUND((SUM(freight_cost_orderline) / SUM(unit_price * shipped_qty_orderline)) * 100.00,2) 
+	   SUM(freight_cost_orderline) AS freight_cost_order,
+	   SUM(unit_price * shipped_qty_orderline) AS revenue_order,
+	   ROUND((SUM(freight_cost_orderline) / SUM(unit_price * shipped_qty_orderline)) * 100.00,2) 
        AS logistics_cost_rev_pct
 FROM fact_ship_rev_orderline
 GROUP BY order_id, dc_id, channel, week_num;
